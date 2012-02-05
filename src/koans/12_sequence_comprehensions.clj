8f1c108cd81d51e9313aae5d4089d1b28ffda7e0
Yehonathan Sharvit <viebel@gmail.com>
2012-02-05 17:03:50 +0200

    solved sequence_comprehensions

--- src/koans/12_sequence_comprehensions.clj
+++ src/koans/12_sequence_comprehensions.clj
@@ -1,36 +1,32 @@
-(ns koans.12-sequence-comprehensions
-  (:require [koan-engine.core :refer :all]))
-
 (meditations
   "Sequence comprehensions can bind each element in turn to a symbol"
-  (= __
-     (for [x (range 6)]
-       x))
+  (= (range 6)
+     (for [index (range 6)]
+       index))
 
   "They can easily emulate mapping"
   (= '(0 1 4 9 16 25)
-     (map (fn [x] (* x x))
+     (map (fn [index] (* index index))
           (range 6))
-     (for [x (range 6)]
-       __))
+     (for [index (range 6)]
+       (* index index)))
 
   "And also filtering"
   (= '(1 3 5 7 9)
      (filter odd? (range 10))
-     (for [x __ :when (odd? x)]
-       x))
+     (for [index (range 10) :when (odd? index)]
+       index))
 
-  "Combinations of these transformations is trivial"
+  "And they trivially allow combinations of the two transformations"
   (= '(1 9 25 49 81)
-     (map (fn [x] (* x x))
+     (map (fn [index] (* index index))
           (filter odd? (range 10)))
-     (for [x (range 10) :when __]
-       __))
+     (for [index (range 10) :when (odd? index)]
+       (* index index)))
 
-  "More complex transformations simply take multiple binding forms"
+  "More complex transformations can be formed with multiple binding forms"
   (= [[:top :left] [:top :middle] [:top :right]
       [:middle :left] [:middle :middle] [:middle :right]
       [:bottom :left] [:bottom :middle] [:bottom :right]]
-     (for [row [:top :middle :bottom]
-           column [:left :middle :right]]
-       __)))
+       (for [row [:top :middle :bottom] column [:left :middle :right]]
+         [row column])))
