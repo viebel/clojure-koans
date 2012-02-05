0adf3a2c0e6bee1823843a57bd03eb375eadcea5
Yehonathan Sharvit <viebel@gmail.com>
2012-02-05 16:59:27 +0200

    solved lazy_sequences

--- src/koans/11_lazy_sequences.clj
+++ src/koans/11_lazy_sequences.clj
@@ -6,18 +6,18 @@
   (= __ (range 1 5))
 
   "The range starts at the beginning by default"
-  (= __ (range 5))
+  (= (range 0 5) (range 5))
 
   "Only take what you need when the sequence is large"
   (= [0 1 2 3 4 5 6 7 8 9]
-     (take __ (range 100)))
+     (take 10 (range 100)))
 
   "Or limit results by dropping what you don't need"
   (= [95 96 97 98 99]
-     (drop __ (range 100)))
+     (drop 95 (range 100)))
 
   "Iteration provides an infinite lazy sequence"
-  (= __ (take 20 (iterate inc 0)))
+  (= (range 20) (take 20 (iterate inc 0)))
 
   "Repetition is key"
   (= [:a :a :a :a :a :a :a :a :a :a]
